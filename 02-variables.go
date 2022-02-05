bb3e59d24dbc6639c18e2e94b063d8637c64252d
Khusan Tojiboev <khusan.tojiboev@gmail.com>
2022-02-05 11:05:28 +0500

    Changed println to printf

--- 02-variables.go
+++ 02-variables.go
@@ -7,7 +7,7 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = 50
 
-	fmt.Println("Welcome to", conferenceName, "bookin application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
-	fmt.Println("Get your ticket here to attend")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your ticket here to attend\n")
 }
